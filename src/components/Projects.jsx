56af324f6be787a52fb85bc7ff0548dc9c9eb608
vineetsaini007 <vineetsainiii007@gmail.com>
2026-02-20 16:12:12 +0530

    fix

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -10,20 +10,20 @@ const Projects = () => {
       transition={{duration: 0.5}}
       className="my-20 text-center text-4xl">Projects</motion.h1>
       <div>
-        {PROJECTS.map((project, index)=>(
-            <div key={index} className="mb-8 flex flex-wrap lg:justify-center">
+         {PROJECTS.map((project, index)=>(
+            <div key={index} className="mb-8 flex flex-wrap justify-center items-center lg:justify-center">
                 <motion.div 
                 whileInView={{opacity: 1, x:0}}
                 initial={{ opacity: 0, x: -100}}
                 transition={{duration: 1}}
                 className="w-full lg:w-1/4">
                 <a href={project.link} target="_blank" rel="noopener noreferrer">
-        <img
-          src={project.image}
-          alt={project.title}
-          width={250}
-          height={350}
-          className="mb-6 rounded"
+            <img
+            src={project.image}
+            alt={project.title}
+            width={250}
+            height={350}
+            className="mb-6 rounded"
         />
       </a>
                 </motion.div>
